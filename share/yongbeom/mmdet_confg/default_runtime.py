50e97cdf385468f8efa5e50707504fd6a75ff319
ybkwon-T2013 <texpoone@naver.com>
2021-10-12 00:35:14 +0900

    update

---
+++ share/yongbeom/mmdet_confg/default_runtime.py
@@ -0,0 +1,24 @@
+# checkpoint_config = dict(max_keep_ckpts=5, interval=1)
+checkpoint_config = dict(interval=1)
+# yapf:disable
+log_config = dict(
+    interval=50,
+    hooks=[
+        dict(type='TextLoggerHook'),
+        dict(type='WandbLoggerHook',
+            init_kwargs=dict(
+                project='mmdet-htc-hrnetv2p',)),
+        # name='booduck4ai')),
+        # dict(type='TensorboardLoggerHook')
+    ])
+# yapf:enable
+custom_hooks = [dict(type='NumClassCheckHook')]
+
+fp16 = dict(loss_scale=512.)
+seed = 123456
+
+dist_params = dict(backend='nccl')
+log_level = 'INFO'
+load_from = None
+resume_from = None
+workflow = [('train', 1)]
