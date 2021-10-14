e8307b8f9be3cd538520842e1a901d54affde525
simbean <sb9338@naver.com>
2021-10-14 11:04:31 +0000

    20211014 final

---
+++ 20211014/pseudo/custom/dataset_labeling.py
@@ -0,0 +1,117 @@
+# dataset settings
+dataset_type = 'CocoDataset'
+data_root = '../../dataset/'
+
+
+
+classes = ("General trash", "Paper", "Paper pack", "Metal", "Glass", 
+           "Plastic", "Styrofoam", "Plastic bag", "Battery", "Clothing")
+
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+
+albu_train_transforms = [
+    dict(
+        type='ShiftScaleRotate',
+        shift_limit=0.0625,
+        scale_limit=0.0,
+        rotate_limit=0,
+        interpolation=1,
+        p=0.5), 
+    dict(
+        type='RandomBrightnessContrast',
+        brightness_limit=[0.1, 0.3],
+        contrast_limit=[0.1, 0.3],
+        p=0.2),
+    dict(
+        type='RGBShift',
+        r_shift_limit=10,
+        g_shift_limit=10,
+        b_shift_limit=10,
+        p=0.2),
+    dict(type='JpegCompression', quality_lower=85, quality_upper=95, p=0.2),
+    dict(type='ChannelShuffle', p=0.2),
+    #dict(type='CLAHE', p = 0.2),
+    dict(
+        type='OneOf',
+        transforms=[
+            dict(type='Blur', blur_limit=3, p=1.0),
+            dict(type='MedianBlur', blur_limit=3, p=1.0)
+        ],
+        p=0.2),
+]
+
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='Resize', img_scale= (1024,1024), keep_ratio=True),
+    dict(type='RandomFlip', flip_ratio= [0.25, 0.25, 0.25] , direction=['horizontal', 'vertical', 'diagonal']),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(
+        type='Albu',
+        transforms=albu_train_transforms,
+        bbox_params=dict(
+            type='BboxParams',
+            format='pascal_voc',
+            label_fields=['gt_labels'],
+            min_visibility=0.0,
+            filter_lost_elements=True),
+        keymap={
+            'img': 'image',
+            'gt_masks': 'masks',
+            'gt_bboxes': 'bboxes'
+        },
+        update_pad_shape=False,
+        skip_img_without_anno=True),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale= (1024,1024),
+        flip= True,
+        flip_direction = ['horizontal', 'vertical' ,'diagonal'],
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+
+data = dict(
+    samples_per_gpu= 8,
+    workers_per_gpu= 2,
+    train=dict(
+        type=dataset_type,
+        classes = classes,
+        ann_file=data_root + 'labeling_data.json',
+        img_prefix=data_root,
+        pipeline=train_pipeline,
+    ),
+    val=dict(
+        type=dataset_type,
+        classes = classes,
+        ann_file=data_root + 'stratified_valid.json',
+        img_prefix=data_root,
+        pipeline=test_pipeline,
+        ),
+    test=dict(
+        type=dataset_type,
+        classes = classes,
+        ann_file=data_root + 'test.json',
+        img_prefix=data_root,
+        pipeline=test_pipeline))
+
+evaluation = dict(interval=1,
+                  metric='bbox',
+                  classwise = True,
+                  save_best='bbox_mAP')
---
+++ 20211014/pseudo/train_custom.py
@@ -0,0 +1,20 @@
+from mmcv import Config
+from mmdet.datasets import build_dataset
+from mmdet.models import build_detector
+from mmdet.apis import train_detector
+from mmdet.datasets import (build_dataloader, build_dataset,
+                            replace_ImageToTensor)
+import copy
+cfg = Config.fromfile('/opt/ml/detection/model/mmdetection/configs/custom/final.py')
+cfg.seed = 2021
+cfg.gpu_ids = [0]
+cfg.work_dir = './work_dirs/swin_adamw12_epoch_augfinalpseudo2_trash'
+cfg.optimizer_config.grad_clip = dict(max_norm=35, norm_type=2)
+cfg.checkpoint_config = dict(max_keep_ckpts=3, interval=1)
+#cfg.model.roi_head.bbox_head.num_classes = 10
+datasets = [build_dataset(cfg.data.train)]
+model = build_detector(cfg.model,
+                       train_cfg=cfg.get('train_cfg'),
+                       test_cfg=cfg.get('test_cfg'))
+model.init_weights()
+train_detector(model, datasets[0], cfg, distributed=False, validate=True)
